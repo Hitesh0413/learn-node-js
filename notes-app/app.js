47d55b97aa05d10b40322af3b65860b1fbf3bf07
Hitesh Aloney <hiteshaloney75@gmail.com>
2019-11-18 22:57:20 +0530

    Learned Yargs Package,file systems and command line arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,52 @@
 const chalk = require('chalk')
-console.log(chalk.blue.inverse.bold('Success!'))
+const yargs = require('yargs')
+const notes = require('./notes') 
+yargs.command({
+    command:'add',
+    describe:'Add A note.',
+    bulider:{
+        title:{
+            describe:'Add A title : ',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'Add A Description : ',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(argv){
+        notes.addNotes(argv.title,argv.body)
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'Remove a note.',
+    bulider:{
+        title:{
+            describe:'A note to be removed: ',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(argv){
+        console.log('Removing A New Note : '+argv.title)
+        notes.removeNote(argv.title)
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe:'Read a note.',
+    handler:function(){
+        console.log('Reading a note : ')
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'Listing note.'    
+})
+yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,45 @@
-const getNotes = function (){
-    return 'Your notes...'
+const fs = require('fs')
+const addNotes = function (title,body){
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title
+    })
+    console.log(duplicateNotes)
+    if(duplicateNotes==0){
+        notes.push({
+            "title":title,
+            "body":body
+        })
+        console.log('New Note Added !')
+        saveNotes(notes)
+        console.log(loadNotes())
+    }
+    else{
+        console.log('This Title Already Taken')
+    }
 }
 
-module.exports = getNotes;
+const removeNote = function (title){
+    console.log('This Should be deleted : '+title)
+}
+
+const loadNotes = function (){
+    try{
+        const  bufferData = fs.readFileSync('notes.json')
+        const  dataString = bufferData.toString()
+        return  JSON.parse(dataString)
+    }
+    catch(e){
+        return [];
+    }
+}
+
+const saveNotes = function (note){
+    
+    fs.writeFileSync('notes.json',JSON.stringify(note))
+}
+
+module.exports = {
+    "addNotes" : addNotes,
+    "removeNote" : removeNote 
+}
